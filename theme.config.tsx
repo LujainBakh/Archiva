fb2446f41a3bb1b64770ec84731855dbc5d153d2
Lujain <152328743+LujainBakh@users.noreply.github.com>
2024-08-06 11:51:55 +0300

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <img>src=".github/archivalogo.png" alt="archiva logo"</img> <span>Archiva</span>,
+  logo: <img>
+    src=".github/archivalogo.png" alt="archiva logo"
+  </img>
+    <span>Archiva</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
