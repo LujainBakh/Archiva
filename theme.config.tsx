6b0e8af96301f3f7730b8831c387fc2b41288e08
Lujain <152328743+LujainBakh@users.noreply.github.com>
2024-08-06 12:00:48 +0300

    Update theme.config.tsx
    added footer, project name, and removed the contact

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,14 +4,11 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>Archiva</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
+    link: 'https://github.com/LujainBakh/archiva',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'PMO documentation for © Solutions By 42',
   },
 }
 
